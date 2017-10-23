71645b9922ff676f6fa2109cd2e300aa195d434a
Hai Bui <buivnhai@gmail.com>
2017-10-23 13:50:19 +0000

    music.sh: remove kill app before open

--- .bin.symlink/music.sh
+++ .bin.symlink/music.sh
@@ -1,10 +1,6 @@
-#! /bin/sh
+#!/bin/sh
 
 # Launch mpd and ncmpcpp
-if [ $(pgrep -cx ncmpcpp) -gt 0 ] ; then
-    pkill ncmpcpp
-fi
-
 if [ $(pgrep -cx mpd) -gt 0 ] ; then
     urxvt -name ncmpcpp -e ncmpcpp
 else
