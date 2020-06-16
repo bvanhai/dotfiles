f9fbfcd0463a72912b7249acb75c276e31c29b5e
Hai Bui <buivnhai@gmail.com>
2020-06-16 09:41:36 +0000

    ldpi.sh: Edit dpi for thinkpad T460 Monitor

--- .bin.symlink/ldpi.sh
+++ .bin.symlink/ldpi.sh
@@ -20,18 +20,9 @@ xrdb -merge <(echo "Xft.dpi: $OLDDPI")
 
 # To calculate monitor DPI:
 # First, run `xrandr` to get the dimension of the monitor, below is the output
-# for the Acer HD monitor:
-# HDMI-0 connected 1920x1080+3840+0 (normal left inverted right x axis y axis) 531mm x 299mm
-# Calculate DPI:
-# 1920/(53.1/2.54) = 1080/(29.9/2.54) = 91.8 dpi
 #
-# For Samsung 4k monitor
-# DP-0 connected primary 3840x2160+0+0 (normal left inverted right x axis y axis) 520mm x 290mm
-# 3840/(52.0/2.54) = 187.6 dpi
-# 2160/(29.0/2.54) = 189 dpi
-#
-# for Thinkpad T460 HD monitor:
-# 1920x1080 with 339mm x 232.5mm
+# For the Thinkpad T460 HD monitor:
+# eDP1 connected primary 1920x1080+0+0 (normal left inverted right x axis y axis) 310mm x 170mm
 # Calculate DPI:
-# 1920/(33.9/2.54) =143.9 dpi
-# 1080/(23.25/2.54) = 118 dpi
+# 1920/(31.0/2.54) = 157.3 dpi
+# 1080/(17.0/2.54) = 161.4 dpi
